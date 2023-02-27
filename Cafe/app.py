a16b20d9afa26e928bae9d14080632feeb77fdda
Nilbog24 <19342@wc.school.nz>
2023-02-27 15:01:09 +1300

    Second commit

--- Cafe/app.py
+++ Cafe/app.py
@@ -1,6 +1,18 @@
+import sqlite3
+from sqlite3 import Error
 from flask import Flask, render_template
 
+DATABASE = "C:/Users/brian/Documents/DTS_Cafe/smile.db"
 app = Flask(__name__)
+
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 
 @app.route('/')
@@ -10,7 +22,14 @@ def render_homepage():
 
 @app.route('/menu')
 def render_menu_page():
-    return render_template('menu.html')
+    con = create_connection(DATABASE)
+    query = "SELECT name, description, volume, image, price FROM products"
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    con.close()
+    print(product_list)
+    return render_template('menu.html', products=product_list)
 
 
 @app.route('/contact')
