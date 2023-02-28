33aedaa79cc3d347f319b05b54517e00fa5f0da9
Nilbog24 <19342@wc.school.nz>
2023-03-01 10:19:32 +1300

    Third commit

--- Cafe/app.py
+++ Cafe/app.py
@@ -20,16 +20,20 @@ def render_homepage():
     return render_template('home.html')
 
 
-@app.route('/menu')
-def render_menu_page():
+@app.route('/menu/<cat_id>')
+def render_menu_page(cat_id):
     con = create_connection(DATABASE)
-    query = "SELECT name, description, volume, image, price FROM products"
+    query = "SELECT name, description, volume, image, price FROM products WHERE cat_id=?"
+    cur = con.cursor()
+    cur.execute(query, (cat_id, ))
+    product_list = cur.fetchall()
+    query = "SELECT id, name FROM category"
     cur = con.cursor()
     cur.execute(query)
-    product_list = cur.fetchall()
+    category_list = cur.fetchall()
     con.close()
     print(product_list)
-    return render_template('menu.html', products=product_list)
+    return render_template('menu.html', products=product_list, categories=category_list)
 
 
 @app.route('/contact')
